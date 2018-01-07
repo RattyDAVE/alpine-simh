ff62c0a0e61a0e75ba6cb261320bbf7054e708d1
RattyDAVE <dave@pucknell.co.uk>
2018-01-07 12:47:23 +0000

    Create 3b2.sh

---
+++ machines/3b2.sh
@@ -0,0 +1,5 @@
+    #!/bin/ah
+    # Taken From https://loomcom.com/3b2/emulator/ \
+    wget https://loomcom.com/3b2/emulator/hd161_extras.img.gz && \
+    gzip -d hd161_extras.img.gz && \
+    wget https://loomcom.com/3b2/emulator/nvram.bin

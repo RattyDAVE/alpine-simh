8c9f0bde43044e1d7db77f0fe7bf2a49b086dff1
RattyDAVE <dave@pucknell.co.uk>
2018-01-07 22:42:36 +0000

    Delete microvax3900.sh

--- machines/microvax3900/microvax3900.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-curl -L -o OpenVMS_VAX_7.3.img.zip -d "nopassword=1" https://download.cloudatcost.com/download/2zx9xe6s62m4vh2rktnkmnujj
-unzip OpenVMS_VAX_7.3.img.zip
-rm unzip OpenVMS_VAX_7.3.img.zip

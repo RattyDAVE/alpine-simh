259a793f5d60359ebbe2b669a3519f387fd0427f
RattyDAVE <dave@pucknell.co.uk>
2018-01-08 09:30:53 +0000

    Update vax.sh

--- machines/vax/vax.sh
+++ machines/vax/vax.sh
@@ -2,4 +2,4 @@
 
 curl -L -o OpenVMS_VAX_7.3.img.zip -d "nopassword=1" https://download.cloudatcost.com/download/2zx9xe6s62m4vh2rktnkmnujj
 unzip OpenVMS_VAX_7.3.img.zip
-rm unzip OpenVMS_VAX_7.3.img.zip
+rm OpenVMS_VAX_7.3.img.zip

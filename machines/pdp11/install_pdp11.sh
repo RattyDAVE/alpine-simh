7777cbcea13f05c857b85dc8591378be7aedc5ff
RattyDAVE <dave@pucknell.co.uk>
2018-01-12 17:10:04 +0000

    Create install_pdp11.sh

---
+++ machines/pdp11/install_pdp11.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+#wget http://simh.trailing-edge.com/kits/caps11_system.zip
+#unzip caps11_system.zip
+
+#wget http://simh.trailing-edge.com/kits/BASIC_CAPS-11_distribution.zip
+#unzip BASIC_CAPS-11_distribution.zip
+
+#wget http://simh.trailing-edge.com/kits/rt11swre.tar.Z
+#tar xvf rt11swre.tar.Z
+
+#wget http://simh.trailing-edge.com/kits/rtv53swre.tar.Z
+#tar xvf rtv53swre.tar.Z
+
+#wget http://simh.trailing-edge.com/kits/rstsv7gen.tar.Z
+#tar xvf rstsv7gen.tar.Z
+
+wget http://simh.trailing-edge.com/kits/rstsv7swre.tar.Z
+tar xvf rstsv7swre.tar.Z
+
+#wget http://simh.trailing-edge.com/kits/uv5swre.zip
+#unzip uv5swre.zip
+
+#wget http://simh.trailing-edge.com/kits/uv6swre.zip
+#unzip uv6swre.zip
+
+#wget http://simh.trailing-edge.com/kits/uv7swre.zip
+#unzip uv7swre.zip
+

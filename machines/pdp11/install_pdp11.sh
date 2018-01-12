3399a7f90dac5daec7896981055154bc3479666a
RattyDAVE <dave@pucknell.co.uk>
2018-01-12 22:49:54 +0000

    Update install_pdp11.sh

--- machines/pdp11/install_pdp11.sh
+++ machines/pdp11/install_pdp11.sh
@@ -67,7 +67,7 @@ mkdir uv5swre
 cd uv5swre
 wget http://simh.trailing-edge.com/kits/uv5swre.zip
 unzip uv5swre.zip
-echo -e "set cpu 1m\natt rl0 unix_v5_rk.dsk\nb rl0" > pdp11.ini
+echo -e "set cpu U18\natt rl0 unix_v5_rk.dsk\nb rl0" > pdp11.ini
 echo #!/bin/sh > run_pdp11.sh
 echo pdp11 >> run_pdp11.sh
 chmod 755 run_pdp11.sh
@@ -77,7 +77,7 @@ mkdir uv6swre
 cd uv6swre
 wget http://simh.trailing-edge.com/kits/uv6swre.zip
 unzip uv6swre.zip
-echo "set cpu 1m" > pdp11.ini
+echo "set cpu U18" > pdp11.ini
 echo "att rl0 unix0_v6_rk.dsk" >> pdp11.ini
 echo "att rl1 unix1_v6_rk.dsk" >> pdp11.ini
 echo "att rl2 unix2_v6_rk.dsk" >> pdp11.ini
@@ -92,7 +92,7 @@ mkdir uv7swre
 cd uv7swre
 wget http://simh.trailing-edge.com/kits/uv7swre.zip
 unzip uv7swre.zip
-echo "set cpu 1m" > pdp11.ini
+echo "set cpu U18" > pdp11.ini
 echo "att rl0 unix_v7_rl.dsk" >> pdp11.ini
 echo "b rl0" >> pdp11.ini
 echo #!/bin/sh > run_pdp11.sh

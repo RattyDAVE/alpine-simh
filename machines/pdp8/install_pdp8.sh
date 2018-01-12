b02a0ef20775f0b3c0e709add74026eb68dc3ec0
RattyDAVE <dave@pucknell.co.uk>
2018-01-12 16:40:54 +0000

    Create install_pdp8.sh

---
+++ machines/pdp8/install_pdp8.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+#wget http://simh.trailing-edge.com/kits/esixswre.tar.Z
+#tar xvf esixswre.tar.Z
+
+#wget http://simh.trailing-edge.com/kits/foclswre.tar.Z
+#tar xvf foclswre.tar.Z
+
+#wget http://simh.trailing-edge.com/kits/os8swre.tar.Z
+#tar xvf os8swre.tar.Z
+
+wget http://simh.trailing-edge.com/kits/tss8.zip
+unzip tss8.zip
+
+wget http://simh.trailing-edge.com/kits/dms8.zip
+unzip dms8.zip
+
+wget http://simh.trailing-edge.com/kits/caps8_all.zip
+unzip caps8_all.zip

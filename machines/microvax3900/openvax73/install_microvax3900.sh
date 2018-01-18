ef6bae8952058b9b7a7f462e8e644be549cef853
RattyDAVE <dave@pucknell.co.uk>
2018-01-18 10:39:23 +0000

    Update and rename machines/microvax3900/install_microvax3900.sh to machines/microvax3900/openvax73/install_microvax3900.sh

--- machines/microvax3900/openvax73/install_microvax3900.sh
+++ machines/microvax3900/openvax73/install_microvax3900.sh
@@ -3,3 +3,4 @@
 curl -L -o OpenVMS_VAX_7.3.img.zip -d "nopassword=1" https://download.cloudatcost.com/download/2zx9xe6s62m4vh2rktnkmnujj
 unzip OpenVMS_VAX_7.3.img.zip
 rm OpenVMS_VAX_7.3.img.zip
+microvax3900 install_microvax3900.ini

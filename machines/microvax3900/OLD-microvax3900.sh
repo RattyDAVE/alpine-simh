8e20330391e69fec2dc1abacf4d91340d6f50b60
RattyDAVE <dave@pucknell.co.uk>
2018-01-19 08:36:17 +0000

    Update and rename machines/microvax3900/OLD-microvax3900.sh to machines/vax/install-netbsd711.sh

--- machines/microvax3900/OLD-microvax3900.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-wget http://simh.trailing-edge.com/kits/zrdos75.zip
-unzip zrdos75.zip
-rm zrdos75.zip
---
+++ machines/vax/install-netbsd711.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+wget http://ftp.netbsd.org/pub/NetBSD/iso/7.1.1/NetBSD-7.1.1-vax.iso

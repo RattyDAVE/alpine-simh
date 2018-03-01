635d36d29dbb72ec0bf3dfe2b1e3e9e4d847061f
RattyDAVE <dave@pucknell.co.uk>
2018-03-01 12:21:40 +0000

    Update install_3b2.sh

--- machines/3b2/install_3b2.sh
+++ machines/3b2/install_3b2.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 # Taken From https://loomcom.com/3b2/emulator/ \
-wget https://loomcom.com/3b2/emulator/hd161_extras.img.gz && \
-gzip -d hd161_extras.img.gz && \
-wget https://loomcom.com/3b2/emulator/nvram.bin
+#wget https://loomcom.com/3b2/emulator/hd161_extras.img.gz
+#gzip -d hd161_extras.img.gz
+#wget https://loomcom.com/3b2/emulator/nvram.bin
+
+curl -L -o hd161.img.gz -d "nopassword=1" https://download.cloudatcost.com/download/npbnp1q1qdhb1lexof8mvxsog
+gzip -d hd161.img.gz

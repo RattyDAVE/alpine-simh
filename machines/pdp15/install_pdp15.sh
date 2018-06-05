e295a56875a6d38f220dcb430eff0fcc7b0f0fe6
RattyDAVE <dave@pucknell.co.uk>
2018-06-05 11:19:46 +0100

    Update install_pdp15.sh

--- machines/pdp15/install_pdp15.sh
+++ machines/pdp15/install_pdp15.sh
@@ -15,14 +15,12 @@ mkdir adss15
 cd adss15
 wget http://simh.trailing-edge.com/kits/adss15.zip
 unzip adss15.zip
-
 echo "load dec-15u.rim 77637" > pdp15.ini
 echo "ex pc" >> pdp15.ini
 echo "attach dt adss15_32k.dtp" >> pdp15.ini
 echo "set lpt disa" >> pdp15.ini
 echo "set lp9 ena" >> pdp15.ini
 echo "run" >> pdp15.ini
-
 echo #!/bin/sh > run_pdp15.sh
 echo "pdp15 pdp15.ini" >> run_pdp15.sh
 chmod 755 run_pdp15.sh
@@ -32,17 +30,9 @@ mkdir dos15
 cd dos15
 wget http://simh.trailing-edge.com/kits/dos15.zip
 unzip dos15.zip
-
 echo #!/bin/sh > run_pdp15.sh
 echo "pdp15 dos.ini" >> run_pdp15.sh
 chmod 755 run_pdp15.sh
-cd ..
-
-
-mkdir xvmdos
-cd xvmdos
-wget http://simh.trailing-edge.com/kits/xvmdos.zip
-unzip xvmdos.zip
 cd ..
 
 mkdir xvmrsx
@@ -51,9 +41,26 @@ wget http://simh.trailing-edge.com/kits/xvmrsx_simh_kit.zip
 unzip xvmrsx_simh_kit.zip
 mv xvmrsx_simh_kit/* .
 rm -R xvmrsx_simh_kit
-
 echo #!/bin/sh > run_pdp15.sh
 echo "pdp15 xvmrsx_boot.ini" >> run_pdp15.sh
 chmod 755 run_pdp15.sh
 cd ..
+
+mkdir xvmrsx
+cd xvmrsx
+wget http://simh.trailing-edge.com/kits/xvmrsx_simh_kit.zip
+unzip xvmrsx_simh_kit.zip
+mv xvmrsx_simh_kit/* .
+rm -R xvmrsx_simh_kit
+echo #!/bin/sh > run_pdp15.sh
+echo "pdp15 xvmrsx_boot.ini" >> run_pdp15.sh
+chmod 755 run_pdp15.sh
+cd ..
+
+mkdir xvmdos
+cd xvmdos
+wget http://simh.trailing-edge.com/kits/xvmdos.zip
+unzip xvmdos.zip
+cd ..
+
 

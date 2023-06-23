35549ef5a37e1dd27a6eb74decf1d56b9278ea4a
RattyDAVE <dave@pucknell.co.uk>
2023-06-23 09:01:10 +0100

    Update install_3b2.sh

--- machines/3b2/install_3b2.sh
+++ machines/3b2/install_3b2.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 echo Taken From https://loomcom.com/3b2/emulator/
-wget https://static.loomcom.com/3b2/emulator/hd161_extras.img.gz
+#wget https://static.loomcom.com/3b2/emulator/hd161_extras.img.gz
 #wget https://loomcom.com/3b2/emulator/hd161_extras.img.gz
-gzip -d hd161_extras.img.gz
+#gzip -d hd161_extras.img.gz
 #wget https://loomcom.com/3b2/emulator/nvram.bin
-wget https://static.loomcom.com/3b2/emulator/nvram.bin
+#wget https://static.loomcom.com/3b2/emulator/nvram.bin
+
+# https://github.com/RattyDAVE/alpine-simh/issues/4
+# Thanks to Chris (https://github.com/crdotson)
+wget https://archives.loomcom.com/3b2/emulator/extras_gnu_src.img.gz
+gzip -d extras_gnu_src.img.gz
+wget https://archives.loomcom.com/3b2/emulator/700/nvram.bin
+
 
 #curl -L -o hd161.img.gz -d "nopassword=1" https://download.cloudatcost.com/download/npbnp1q1qdhb1lexof8mvxsog
 #gzip -d hd161.img.gz
